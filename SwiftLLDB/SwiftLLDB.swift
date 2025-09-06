7e64f6a0bbfc704b8acfb4a325a68b5a6749949c
Max Van den Eynde <maxvdec@gmail.com>
2025-09-06 20:38:19 +0200

    Initial Commit

---
+++ SwiftLLDB/SwiftLLDB.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftLLDB.swift
+//  SwiftLLDB
+//
+//  Created by Max Van den Eynde on 6/9/25.
+//
+
+import Foundation
+
---
+++ SwiftLLDBTests/SwiftLLDBTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLLDBTests.swift
+//  SwiftLLDBTests
+//
+//  Created by Max Van den Eynde on 6/9/25.
+//
+
+import Testing
+@testable import SwiftLLDB
+
+struct SwiftLLDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
